7c4d154f166a42930ec34d63ec4fa617bb33afa0
Noriishere <noriyamashin@gmail.com>
2025-10-31 08:53:11 +0700

    using namespace and access modifiers

--- property-constant/LuasLingkaran.php
+++ property-constant/LuasLingkaran.php
@@ -1,13 +1,29 @@
 <?php
 
-class LuasLingkaran {
+namespace App\math;
+class lingkaran{
 
-    public const phi = 3.14;
-    public int $jari;
+    private int $r;
+    public const PHI = 3.14;
+    public int $hasil;
+    protected string $nama;
+    public function __construct($nama = "Roda", $r=1){
+        $this->r = $r;
+        $this->nama = $nama;
+    }
+    public function tampil(){
+        $this->hasil = lingkaran::PHI * $this->r * $this->r;
+        echo "Lingkaran {$this->nama} ini Hasilnya adalah: {$this->hasil}";
+    }
+    public static function testing(){
+        echo '<br>';
+        echo "ini testing Static";
+    }
+    public function __destruct(){
+        echo '<br>';
+        echo "Bisa tapi mati ";
+    }
 }
 
-$lingkaran = new LuasLingkaran();
-$lingkaran->jari = 12;
 
-$rumus = LuasLingkaran::phi * ($lingkaran->jari * $lingkaran->jari);
-echo "Hasilnya adalah: ".$rumus;
+?>
